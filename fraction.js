3b88d995bf84875d48a096b1cca36e9d4d287078
Robert Eisele <robert@xarg.org>
2015-08-23 22:16:36 +0200

    Improved cycle offset calculation

--- fraction.js
+++ fraction.js
@@ -266,11 +266,18 @@
     };
 
     var cycleStart = function(n, d, len) {
+
+        var rem1 = 1;
+        var rem2 = modpow(10, len, d);
         
-        for (var s = 0; s < 300; s++) { // s < ~log10(Number.MAX_VALUE)
+        for (var t = 0; t < 300; t++) { // s < ~log10(Number.MAX_VALUE)
             // Solve 10^s == 10^(s+t) (mod d)
-            if (modpow(10, s, d) === modpow(10, s + len, d))
-                return s;
+
+            if (rem1 === rem2)
+                return t;
+
+            rem1 = rem1 * 10 % d;
+            rem2 = rem2 * 10 % d;
         }
         return 0;
     };
