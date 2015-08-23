5105f728c7c220407cc732414aa9eb44b4b5ca1a
Robert Eisele <robert@xarg.org>
2015-08-23 17:19:35 +0200

    Added some notes about cyclic numbers

--- fraction.js
+++ fraction.js
@@ -40,6 +40,11 @@
 (function(root) {
 
     "use strict";
+    
+    // Maximum search depth for cyclic rational numbers. 2000 should be more than enough. 
+    // Example: 1/7 = 0.(142857) has 6 repeating decimal places.
+    // When number gets reduced, long cycles will not be detected and toString() only gets the first 10 digits
+    var MAX_CYCLE_LEN = 2000;
 
     // Parsed data to avoid calling "new" all the time
     var P = {
@@ -243,14 +248,19 @@
         
         if (d === 1) // Catch non-cyclic numbers
             return 0;
+            
+        // If we would like to compute really large numbers quicker, we could make use of Fermat's little theorem:
+        // 10^(d-1) % d == 1
+        // However, we don't need such large numbers and MAX_CYCLE_LEN should be the capstone, 
+        // as we want to translate the numbers to strings.
 
         var rem = 10 % d;
 
         for (var t = 1; rem !== 1; t++) {
             rem = rem * 10 % d;
 
-            if (t > 2000) // This check is not necessary per se, but for really large numbers the algorithm runs into an endless loop. There might be a better fix
-                return 0;
+            if (t > MAX_CYCLE_LEN)
+                return 0; // Returning 0 here means that we don't print it as a cyclic number. It's likely that the answer is `d-1`
         }
         return t;
     };
