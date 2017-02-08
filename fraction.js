4a12e0540610ab67a8e09fbd2327152f61d91f38
Robert Eisele <robert@xarg.org>
2017-02-09 00:08:45 +0100

    pushed version

--- fraction.js
+++ fraction.js
@@ -1,5 +1,5 @@
 /**
- * @license Fraction.js v3.3.1 09/09/2015
+ * @license Fraction.js v4.0.0 09/09/2015
  * http://www.xarg.org/2014/03/rational-numbers-in-javascript/
  *
  * Copyright (c) 2015, Robert Eisele (robert@xarg.org)
