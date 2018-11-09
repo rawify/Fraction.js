7cc957b33c6eb3cb685c602bac961a7ceb6c9a0d
Robert Eisele <robert@xarg.org>
2018-11-09 14:25:04 +0100

    Update fraction.d.ts

--- fraction.d.ts
+++ fraction.d.ts
@@ -43,7 +43,7 @@ export default class Fraction {
 
   inverse(): Fraction;
   
-  simmplify(eps?: number): Fraction;
+  simplify(eps?: number): Fraction;
   
   equals(n: number | string | Fraction): boolean;
   compare(n: number | string | Fraction): number;
