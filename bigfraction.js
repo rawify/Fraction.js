3ae4f0a42eb279d47cf6aa2c6f6dc90d1513a143
Robert Eisele <robert@xarg.org>
2019-01-03 10:43:18 +0100

    Finished #30

--- bigfraction.js
+++ bigfraction.js
@@ -1,5 +1,5 @@
 /**
- * @license Fraction.js v4.0.11 09/09/2015
+ * @license Fraction.js v4.0.12 09/09/2015
  * http://www.xarg.org/2014/03/rational-numbers-in-javascript/
  *
  * Copyright (c) 2015, Robert Eisele (robert@xarg.org)
